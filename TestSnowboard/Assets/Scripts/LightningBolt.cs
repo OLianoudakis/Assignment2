265998c133c6c3875cf8cefc2c66b196a6d96715
OLianoudakis <gusliaor@student.gu.se>
2019-11-18 14:21:53 +0100

    first level and new models

--- TestSnowboard/Assets/Scripts/LightningBolt.cs
+++ TestSnowboard/Assets/Scripts/LightningBolt.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class LightningBolt : MonoBehaviour
 {
+    public Transform hook1 = null;
+    public Transform hook2 = null;
+    public NeedToSet choice = NeedToSet.no;
 	
 	// Use this for initialization
 	void Awake ()
@@ -10,7 +13,19 @@ public class LightningBolt : MonoBehaviour
 		Material newMat = GetComponent<LineRenderer>().material;
 		newMat.SetFloat("_StartSeed",Random.value*1000);
 		GetComponent<LineRenderer>().material = newMat;
+
+        if (choice == NeedToSet.yes)
+        {
+            GetComponent<LineRenderer>().SetPosition(0, hook1.position);
+            GetComponent<LineRenderer>().SetPosition(1, hook2.position);
+        }
 		
 	}
+
+    public enum NeedToSet
+    {
+        yes,
+        no
+    }
 }
 
